cc8159265cce35ba045c512f4938eb69c7f95af2
Lenart Kos <koslenart@gmail.com>
2018-07-19 15:15:10 +0200

    Base release

---
+++ CFakeViewer/F.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace CFakeViewer
+{
+    /// <summary>
+    /// A class for functions
+    /// </summary>
+    class F
+    {
+        public void ToggleFullscreen(MainWindow window)
+        {
+            if (window.WindowState == WindowState.Normal)
+            {
+                window.WindowStyle = WindowStyle.None;
+                window.WindowState = WindowState.Maximized;
+                window.fullscreenBtn.Content = "Normal";
+            }
+            else
+            {
+                window.WindowState = WindowState.Normal;
+                window.WindowStyle = WindowStyle.SingleBorderWindow;
+                window.fullscreenBtn.Content = "Fullscreen";
+            }
+        }
+        public async Task<BitmapImage> GetImageAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            using (Stream stream = await client.GetStreamAsync(url))
+            using (MemoryStream ms = new MemoryStream())
+            {
+                await stream.CopyToAsync(ms);
+                ms.Position = 0;
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = ms;
+                image.EndInit();
+                return image;
+            }
+        }
+    }
+}
--- CFakeViewer/MainWindow.xaml.cs
+++ CFakeViewer/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,215 @@ namespace CFakeViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private F f { get; set; }
+        ImageWithName current;
+        ImageWithName prev;
+        ImageWithName next;
+        int counter = 0;
+        string mainUrl = "http://cfake.com/show/p";
         public MainWindow()
         {
             InitializeComponent();
+            f = new F();
         }
+
+        private void FullscreenClick(object sender, RoutedEventArgs e)
+        {
+            f.ToggleFullscreen(this);
+        }
+        private void ColapseClick(object sender, RoutedEventArgs e)
+        {
+            GridOverlay.Opacity = 0.0;
+            expandBtn.Opacity = 0.5;
+            GridOverlay.IsEnabled = false;
+            Canvas.SetZIndex(GridOverlay, 999);
+            Canvas.SetZIndex(expandBtn, 1000);
+        }
+        private void ExpandClick(object sender, RoutedEventArgs e)
+        {
+            expandBtn.Opacity = 0.0;
+            GridOverlay.Opacity = 0.5;
+            GridOverlay.IsEnabled = true;
+            Canvas.SetZIndex(expandBtn, 999);
+            Canvas.SetZIndex(GridOverlay, 1000);
+        }
+
+        private void KeyboardClick(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape: f.ToggleFullscreen(this); break;
+                case Key.F: f.ToggleFullscreen(this); break;
+                case Key.Up: App.Current.Shutdown(); break;
+                case Key.Left: ShowPrevAsync(); break;
+                case Key.Right: ShowNextAsync(); break;
+                /*case Key.T: TestAsync(); break;*/
+            }
+        }
+
+        private async void ShowNextAsync()
+        {
+            prev = current;
+            if (next != null)
+                current = next;
+            else
+                MessageBox.Show("No more images");
+
+            imageBox.Source = current.Image;
+            nameButton.Content = current.Name;
+            try
+            {
+                ulong i = current.ID + 1;
+                /*if (favNewest)
+                {
+                    i = i % 30;
+                    if (i == 0)
+                    {
+                        Names = await GetNames(++currentList);
+                    }
+
+                    while (true)
+                    {
+                        while (true)
+                        {
+                            if (fav.Find(s => s == Names[(int)i]) != null)
+                            {
+                                i = (ulong)currentList * 30 + i;
+                                goto FoundNext;
+                            }
+                            else
+                            {
+                                i++;
+                                if (i == 30)
+                                {
+                                    i = 0;
+                                    currentList++;
+                                    break;
+                                }
+                            }
+                        }
+                        Names = await GetNames(currentList);
+                    }
+                }
+            FoundNext:*/
+                next = await GetImageWithNameAsync(i);
+            }
+            catch
+            {
+                next = null;
+            }
+        }
+
+        private async void ShowPrevAsync()
+        {
+            if (prev != null)
+            {
+                next = current;
+                current = prev;
+
+                imageBox.Source = current.Image;
+                nameButton.Content = current.Name;
+                if (current.ID != 0)
+                {
+                    ulong i = current.ID - 1;
+                    /*if (favNewest)
+                    {
+                        i = i % 30;
+                        if (i == 0 && currentList != 0)
+                        {
+                            Names = await GetNames(--currentList);
+                        }
+
+                        while (true)
+                        {
+                            while (true)
+                            {
+                                if (fav.Find(s => s == Names[(int)i]) != null)
+                                {
+                                    i = (ulong)currentList * 30 + i;
+                                    goto FoundNext;
+                                }
+                                else
+                                {
+                                    if (i == 0)
+                                    {
+                                        i = 29;
+                                        currentList--;
+                                        break;
+                                    }
+                                    else
+                                        i--;
+                                }
+                            }
+                            if (currentList == -1)
+                            {
+                                prev = null;
+                                return;
+                            }
+                            Names = await GetNames(currentList);
+                        }
+                    }
+                FoundNext:*/
+                    prev = await GetImageWithNameAsync(i);
+                }
+                else
+                    prev = null;
+            }
+        }
+
+        private async Task<ImageWithName> GetImageWithNameAsync(ulong v)
+        {
+            string url = mainUrl + v.ToString();
+            string pageSource;
+
+            using (HttpClient client = new HttpClient())
+                pageSource = await client.GetStringAsync(url);
+
+            pageSource = pageSource.Replace("\n", "");
+            pageSource = pageSource.Replace("\t", "");
+
+            string nameCompare = "<td width=\"100%\"><a href=\"/picture/";
+            int nameIndex = pageSource.IndexOf(nameCompare) + nameCompare.Length;
+            pageSource = pageSource.Remove(0, nameIndex);
+            string name = pageSource.Substring(0, pageSource.IndexOf("/"));
+            name = name.Replace('_', ' ');
+            if (name == "")
+                return null;
+
+            string imgCompare = "<img src=\"/medias/photos/";
+            int imgIndex = pageSource.IndexOf(imgCompare) + imgCompare.Length;
+            pageSource = pageSource.Remove(0, imgIndex);
+            string filename = pageSource.Substring(0, pageSource.IndexOf('"'));
+            string imgUrl = "http://cfake.com/medias/photos/" + filename;
+
+            var image = await f.GetImageAsync(imgUrl);
+
+            return new ImageWithName(image, name, filename, v);
+        }
+
+        private async void LoadAsync(object sender, RoutedEventArgs e)
+        {
+            current = await GetImageWithNameAsync(0);
+            next = await GetImageWithNameAsync(1);
+
+            imageBox.Source = current.Image;
+            nameButton.Content = current.Name;
+        }
+    }
+
+    public class ImageWithName
+    {
+        public ImageWithName(BitmapImage image, string name, string filename, ulong id)
+        {
+            Image = image;
+            Name = name;
+            FileName = filename;
+            ID = id;
+        }
+
+        public BitmapImage Image { get; set; }
+        public String Name { get; set; }
+        public String FileName { get; set; }
+        public ulong ID { get; set; }
     }
 }
